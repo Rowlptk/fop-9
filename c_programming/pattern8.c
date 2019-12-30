0083565f0f0b47b27e95e0019dbc4d0c97cc6939
Rahul Pathak <rahulzha93@gmail.com>
2019-12-30 15:02:29 +0545

    pattern

---
+++ c_programming/pattern8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    int i, j;
+    for(i=65;i<=69;i++)
+    {
+        for(j=65;j<=i;j++)
+        {
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+}
+
+array ==> one Dimensional and two dimensional ==> matrix
